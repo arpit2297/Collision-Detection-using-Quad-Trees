c942739a9c1c9387944c8f3d7f861b7751e9c2a3
Arpit <arpit.chess@gmail.com>
2017-05-28 21:25:53 -0400

    Randomly generating n objects with random velocities

--- Desktop/cd-qd/graphics.py
+++ Desktop/cd-qd/graphics.py
@@ -1,21 +1,19 @@
 import pygame
 import math
+import random
+
 pygame.init()
 
 
 class Point:
-	def __init__(self,x,y):
+	def __init__(self,x,y, fx = 1, fy = 1):
 		self.x = x
 		self.y = y
+		self.fx = fx
+		self.fy = fy
 
 	def addPoint(self,p):
 		return Point(self.x + p.x, self.y + p.y)
-
-	def getX(self):
-		return self.x		
-
-	def getY(self):
-		return self.y
 
 	def setX(self,x):
 		self.x = x
@@ -23,6 +21,8 @@ class Point:
 	def setY(self,y):
 		self.y = y
 
+	def updateVelocity(self,velocity):
+		self.velocity = velocity	
 
 class Window:
 	def __init__(self,windowScreen, width, height):
@@ -42,6 +42,7 @@ class Window:
 
 class Color:
 	WHITE = (255,255,255)
+	GREEN = (0,255,0)
 	BLACK = (0,0,0)
 	RED = (255,0,0)
 	BLUE = (0,0,255)
@@ -54,14 +55,22 @@ def setWindowCaption(caption):
 	pygame.display.set_caption(caption)
 
 def main():
-	flipx = 1
-	flipy = 1
 	window = Window(pygame.display.set_mode([700,500]), 700, 500)
+	limit = 25
 
 	setWindowCaption("Collision Detection Using Quad Trees")
 
-	ball = Point(50,50)
-	velocity = Point(3,3) # increase x,y by 3 always
+	objects = list()
+
+	for i in range(limit):
+		x = int(round(random.uniform(0,window.width - 1)))
+		y = int(round(random.uniform(0, window.height - 1)))
+		ball = Point(x,y,1,1)
+		vx = int(round(random.uniform(1,6)));
+		vy = int(round(random.uniform(1,6)));
+		velocity = Point(vx,vy)
+		ball.updateVelocity(velocity)
+		objects.append(ball)
 
 	done = False
 	clock = pygame.time.Clock()
@@ -71,18 +80,19 @@ def main():
 				done = True
 
 		# draw here
-		window.getWindowReference().fill(Color.WHITE)
+		window.getWindowReference().fill(Color.BLACK)
 
-		if ball.getX() == window.getWidth() or ball.getX() == 0:
-			flipx = -1 * flipx
+		for ball in objects:
+			if ball.x == window.getWidth() or ball.x == 0:
+				ball.fx = -1 * ball.fx
 
-		if ball.getY() == window.getHeight() or ball.getY() == 0:
-			flipy = -1 * flipy
+			if ball.y == window.getHeight() or ball.y == 0:
+				ball.fy = -1 * ball.fy
 
-		ball.setX(max(min(ball.getX() + velocity.getX()*flipx, window.getWidth()),0))
-		ball.setY(max(min(ball.getY() + velocity.getY()*flipy, window.getHeight()),0))
+			ball.setX(max(min(ball.x + ball.velocity.x*ball.fx, window.getWidth()),0))
+			ball.setY(max(min(ball.y + ball.velocity.y*ball.fy, window.getHeight()),0))
 
-		drawPointSizedObject(window.getWindowReference(),Color.BLACK,[ball.getX(),ball.getY()],4,0)
+			drawPointSizedObject(window.getWindowReference(),Color.BLUE,[ball.x,ball.y],4,0)
 		
 		pygame.display.flip()
 
